b4eedf053c5cb469f07ac000814c60980d19ad30
richard <richardparkdragon@gmail.com>
2018-11-12 14:57:39 +0800

    Initial Commit

---
+++ jhzxProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  jhzxProject
+//
+//  Created by 权威 on 2018/11/12.
+//  Copyright © 2018年 Smith Tylor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ jhzxProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jhzxProject
+//
+//  Created by 权威 on 2018/11/12.
+//  Copyright © 2018年 Smith Tylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
